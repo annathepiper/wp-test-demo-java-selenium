f7cc292b6881576d95d4b57e1e583a31ea2db126
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-30 15:04:43 -0800

    Adding assorted property file strings for homepage tests

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,14 +1,13 @@
 import com.codeborne.selenide.Configuration;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
 import java.util.ResourceBundle;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
 
 /**
  * BaseTest
  * @author Angela Korra'ti
  *
- * Last updated 1/29/2019
+ * Last updated 1/30/2019
  * This is the master class for the wptestdemoselenium suite. Does the necessary setup for all the other classes.
  */
 public class BaseTest {
@@ -16,16 +15,30 @@ public class BaseTest {
     public String protocol;
     public String host;
     public String wpBaseUri;
+    public String siteTitleClass;
+    public String siteTitle;
+    public String siteDescriptionClass;
+    public String siteDescription;
+    public String menuId;
+    public String contentId;
 
     // Resource bundle we're using to pull all the property strings out of
     private static ResourceBundle rb = ResourceBundle.getBundle("wp-test-demo-selenium");
 
-    @BeforeClass
+    @BeforeTest
     public void classSetup() {
-        // Get the needed properties out of the file
+        // Get the needed properties out of the file to build some URIs to test
         protocol = rb.getString("protocol");
         host = rb.getString("host");
         wpBaseUri = String.format("%s://%s", protocol, host);
+
+        // Assorted values we'll need for test cases
+        siteTitleClass = rb.getString("siteTitleClass");
+        siteTitle = rb.getString("siteTitle");
+        siteDescriptionClass = rb.getString("siteDescriptionClass");
+        siteDescription = rb.getString("siteDescription");
+        menuId=rb.getString("menuId");
+        contentId=rb.getString("contentId");
 
         // Set our Selenium locale for Selenide to use.
         Configuration.remote="http://localhost:4444/wd/hub";
