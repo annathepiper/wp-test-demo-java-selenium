93279fdb1bf1beeffc445e3e8857e68ed5a1cc8a
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-31 15:08:02 -0800

    Changed properties file strings to be static so they'll be accessible by all test classes; also added strings for Menu test cases

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,42 +1,57 @@
 import com.codeborne.selenide.Configuration;
-import org.testng.annotations.BeforeTest;
-
 import java.util.ResourceBundle;
 
 /**
  * BaseTest
  * @author Angela Korra'ti
  *
- * Last updated 1/30/2019
+ * Last updated 1/31/2019
  * This is the master class for the wptestdemoselenium suite. Does the necessary setup for all the other classes.
  */
 public class BaseTest {
     // This is the location for the Selenium grid
-    public String seleniumHost;
+    public static String seleniumHost;
 
     // Use these to set up the base URI for our testing
-    public String protocol;
-    public String host;
-    public String wpBaseUri;
+    public static String protocol;
+    public static String host;
+    public static String wpBaseUri;
 
     // Assorted strings we need for the test cases
-    public String siteTitleClass;
-    public String siteTitle;
-    public String siteDescriptionClass;
-    public String siteDescription;
-    public String menuId;
-    public String contentId;
-    public String primaryContentId;
-    public String secondaryContentId;
-    public String footerId;
-    public String footerSocialMenuId;
-    public String footerSiteInfoClass;
+    public static String siteTitleClass;
+    public static String siteTitle;
+    public static String siteDescriptionClass;
+    public static String siteDescription;
+    public static String menuId;
+    public static String contentId;
+    public static String primaryContentId;
+    public static String secondaryContentId;
+    public static String footerId;
+    public static String footerSocialMenuId;
+    public static String footerSiteInfoClass;
+    public static String menuHomeXPath;
+    public static String menuHomeText;
+    public static String menuHomeLink;
+    public static String menuAboutXPath;
+    public static String menuAboutText;
+    public static String menuAboutLink;
+    public static String menuBooksXPath;
+    public static String menuBooksText;
+    public static String menuBooksLink;
+    public static String menuBlogXPath;
+    public static String menuBlogText;
+    public static String menuBlogLink;
+    public static String menuContactXPath;
+    public static String menuContactText;
+    public static String menuContactLink;
+    public static String menuStoreXPath;
+    public static String menuStoreText;
+    public static String menuStoreLink;
 
     // Resource bundle we're using to pull all the property strings out of
     private static ResourceBundle rb = ResourceBundle.getBundle("wp-test-demo-selenium");
 
-    @BeforeTest
-    public void classSetup() {
+    public static void suiteSetup() {
         // Get the location for the Selenium grid
         seleniumHost = rb.getString("seleniumHost");
 
@@ -57,6 +72,24 @@ public class BaseTest {
         footerId = rb.getString("footerId");
         footerSocialMenuId = rb.getString("footerSocialMenuId");
         footerSiteInfoClass = rb.getString("footerSiteInfoClass");
+        menuHomeXPath = rb.getString("menuHomeXPath");
+        menuHomeText = rb.getString("menuHomeText");
+        menuHomeLink = rb.getString("menuHomeLink");
+        menuAboutXPath = rb.getString("menuAboutXPath");
+        menuAboutText = rb.getString("menuAboutText");
+        menuAboutLink = rb.getString("menuAboutLink");
+        menuBooksXPath = rb.getString("menuBooksXPath");
+        menuBooksText = rb.getString("menuBooksText");
+        menuBooksLink = rb.getString("menuBooksLink");
+        menuBlogXPath = rb.getString("menuBlogXPath");
+        menuBlogText = rb.getString("menuBlogText");
+        menuBlogLink = rb.getString("menuBlogLink");
+        menuContactXPath = rb.getString("menuContactXPath");
+        menuContactText = rb.getString("menuContactText");
+        menuContactLink = rb.getString("menuContactLink");
+        menuStoreXPath = rb.getString("menuStoreXPath");
+        menuStoreText = rb.getString("menuStoreText");
+        menuStoreLink = rb.getString("menuStoreLink");
 
         // Set our Selenium locale for Selenide to use.
         Configuration.remote=seleniumHost;
