d683feff95c2531bcb2c7c41bd742cf5a329227d
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-30 16:34:37 -0800

    Additional strings for homepage tests, as well as a string for seleniumHost

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -11,22 +11,35 @@ import java.util.ResourceBundle;
  * This is the master class for the wptestdemoselenium suite. Does the necessary setup for all the other classes.
  */
 public class BaseTest {
+    // This is the location for the Selenium grid
+    public String seleniumHost;
+
     // Use these to set up the base URI for our testing
     public String protocol;
     public String host;
     public String wpBaseUri;
+
+    // Assorted strings we need for the test cases
     public String siteTitleClass;
     public String siteTitle;
     public String siteDescriptionClass;
     public String siteDescription;
     public String menuId;
     public String contentId;
+    public String primaryContentId;
+    public String secondaryContentId;
+    public String footerId;
+    public String footerSocialMenuId;
+    public String footerSiteInfoClass;
 
     // Resource bundle we're using to pull all the property strings out of
     private static ResourceBundle rb = ResourceBundle.getBundle("wp-test-demo-selenium");
 
     @BeforeTest
     public void classSetup() {
+        // Get the location for the Selenium grid
+        seleniumHost = rb.getString("seleniumHost");
+
         // Get the needed properties out of the file to build some URIs to test
         protocol = rb.getString("protocol");
         host = rb.getString("host");
@@ -37,10 +50,15 @@ public class BaseTest {
         siteTitle = rb.getString("siteTitle");
         siteDescriptionClass = rb.getString("siteDescriptionClass");
         siteDescription = rb.getString("siteDescription");
-        menuId=rb.getString("menuId");
-        contentId=rb.getString("contentId");
+        menuId = rb.getString("menuId");
+        contentId = rb.getString("contentId");
+        primaryContentId = rb.getString("primaryContentId");
+        secondaryContentId = rb.getString("secondaryContentId");
+        footerId = rb.getString("footerId");
+        footerSocialMenuId = rb.getString("footerSocialMenuId");
+        footerSiteInfoClass = rb.getString("footerSiteInfoClass");
 
         // Set our Selenium locale for Selenide to use.
-        Configuration.remote="http://localhost:4444/wd/hub";
+        Configuration.remote=seleniumHost;
     }
 }
