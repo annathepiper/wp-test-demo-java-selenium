4839fe210e7921ccbe4ff04ef93845524e80af71
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-24 16:48:06 -0700

    Initial stub for WPMenu
    I'll be filling in this code soon. Porting over from Python.

---
+++ src/main/java/WPMenu.java
@@ -0,0 +1,9 @@
+/**
+ * WPMenu
+ * @author Angela Korra'ti
+ *
+ * Last updated 4/24/2019
+ * This is the WPMenu helper class that defines the structure of a menu page object.
+ */
+public class WPMenu extends BaseTest {
+}
