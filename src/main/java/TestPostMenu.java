3c264ab10fa23430771f1f9f92f2b01ba2d76c8f
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-24 16:46:16 -0700

    Correcting URI to test
    Since this is a Post class, correcting the URI that needs testing.

--- src/main/java/TestPostMenu.java
+++ src/main/java/TestPostMenu.java
@@ -18,6 +18,6 @@ public class TestPostMenu extends TestMenu {
     @BeforeMethod
     public void methodSetup() {
         suiteSetup();
-        open(wpBaseUri);
+        open(wpPostUri);
     }
 }
