7a25b5979d4c99e5103618dede063315bec6960a
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-24 16:41:23 -0700

    TestFooter refactor
    Refactoring this class as an abstract parent class that child classes can implement.

--- src/main/java/TestFooter.java
+++ src/main/java/TestFooter.java
@@ -1,170 +1,162 @@
-import com.codeborne.selenide.SelenideElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
 /**
  * TestFooter
  * @author Angela Korra'ti
  *
- * Last updated 4/12/2019
- * This test class tests that the expected items appear in the footer, and that they link to the expected places.
+ * Last updated 4/24/2019
+ * This test class tests that the expected items appear in the footer, and that they link to the expected places. It
+ * serves as the parent class for test classes that hit specific pages.
  */
-public class TestFooter extends BaseTest {
-
-    /**
-     * Setup
-     * This method opens up the homepage of the test site so we can do tests on it.
-     */
-    @BeforeMethod
-    public void methodSetup() {
-        suiteSetup();
-        open(wpBaseUri);
-    }
-
+abstract class TestFooter extends BaseTest {
+    abstract void methodSetup();
 
     /**
      * TestFooterSiteTitleLink
      * Verify that the site title link is present, visible, and has the correct text.
      */
-    @Test
-    public void TestFooterSiteTitleLink() {
-        $(byXpath(footerSiteTitleXPath))
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(text(siteTitle));
-    }
+    abstract void TestFooterSiteTitleLink();
 
     /**
      * TestFooterSiteTitleLinkClick
      * Verify that if you click on the site title link, you land on the correct page (the homepage).
      */
-    @Test
-    public void TestFooterSiteTitleLinkClick() {
-        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
-        $(byXpath(footerSiteTitleXPath)).scrollTo().click();
-        Assert.assertEquals(url(),menuHomeLink);
-    }
+    abstract void TestFooterSiteTitleLinkClick();
 
     /**
      * TestFooterWPLink
      * Verify that the Wordpress link is present, visible, and has the correct text.
      */
-    @Test
-    public void TestFooterWPLink() {
-        SelenideElement footerWPLink = $(byXpath(footerWPLinkXPath));
-        footerWPLink.should(exist).shouldBe(visible);
-        String actualFooterWPLinkText = footerWPLink.text();
-        Assert.assertEquals(actualFooterWPLinkText, footerWPLinkText,
-                "WordPress footer link doesn't have correct text.");
-    }
+    abstract void TestFooterWPLink();
 
     /**
      * TestFooterWPLinkClick
      * Verify that if you click on the Wordpress link, you land on the correct page (wordpress.org).
      */
-    @Test
-    public void TestFooterWPLinkClick() {
-        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
-        $(byXpath(footerWPLinkXPath)).scrollTo().click();
-        Assert.assertEquals(url(),footerWPLink);
-    }
+    abstract void TestFooterWPLinkClick();
 
     /**
      * TestFooterSocialFacebookLink
      * Verify that the Facebook link is present, visible, and has the correct text.
      */
-    @Test
-    public void TestFooterSocialFacebookLink() {
-        $(byXpath(footerSocialFacebookXPath))
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(text(footerSocialFacebookText));
-    }
+    abstract void TestFooterSocialFacebookLink();
 
     /**
      * TestFooterSocialFacebookLinkClick
      * Verify that if you click on the Facebook link, you land on the correct page (my Facebook account).
      */
-    @Test
-    public void TestFooterSocialFacebookLinkClick() {
-        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
-        $(byXpath(footerSocialFacebookXPath)).scrollTo().click();
-        Assert.assertEquals(url(),footerSocialFacebookLink);
-    }
+    abstract void TestFooterSocialFacebookLinkClick();
 
     /**
      * TestFooterSocialTwitterLink
      * Verify that the Twitter link is present, visible, and has the correct text.
      */
-    @Test
-    public void TestFooterSocialTwitterLink() {
-        $(byXpath(footerSocialTwitterXPath))
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(text(footerSocialTwitterText));
-    }
+    abstract void TestFooterSocialTwitterLink();
 
     /**
      * TestFooterSocialTwitterLinkClick
      * Verify that if you click on the Twitter link, you land on the correct page (my Twitter account).
      */
-    @Test
-    public void TestFooterSocialTwitterLinkClick() {
-        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
-        $(byXpath(footerSocialTwitterXPath)).scrollTo().click();
-        Assert.assertEquals(url(),footerSocialTwitterLink);
-    }
+    abstract void TestFooterSocialTwitterLinkClick();
 
     /**
      * TestFooterSocialGithubLink
      * Verify that the Github link is present, visible, and has the correct text.
      */
-    @Test
-    public void TestFooterSocialGithubLink() {
-        $(byXpath(footerSocialGithubXPath))
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(text(footerSocialGithubText));
-    }
+    abstract void TestFooterSocialGithubLink();
 
     /**
      * TestFooterSocialGithubLinkClick
      * Verify that if you click on the Github link, you land on the correct page (my Github account).
      */
-    @Test
-    public void TestFooterSocialGithubLinkClick() {
-        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
-        $(byXpath(footerSocialGithubXPath)).scrollTo().click();
-        Assert.assertEquals(url(),footerSocialGithubLink);
-    }
+    abstract void TestFooterSocialGithubLinkClick();
 
     /**
      * TestFooterSocialLinkedInLink
      * Verify that the LinkedIn link is present, visible, and has the correct text.
      */
-    @Test
-    public void TestFooterSocialLinkedInLink() {
-        $(byXpath(footerSocialLinkedInXPath))
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(text(footerSocialLinkedInText));
-    }
+    abstract void TestFooterSocialLinkedInLink();
 
     /**
-     * TestFooterSocialLinkedInLinkClick
+     * TestSocialLinkedInLinkClick
      * Verify that if you click on the LinkedIn link, you land on the correct page (my LinkedIn account).
      */
-    @Test
-    public void TestFooterSocialLinkedInLinkClick() {
+    abstract void TestSocialLinkedInLinkClick();
+
+    // HELPER METHODS BEGIN HERE
+    void verifySiteTitleLink(WPFooter wpFooter) {
+        wpFooter.siteTitleElement().should(exist).shouldBe(visible);
+        Assert.assertEquals(wpFooter.siteTitleText(), siteTitle,"Site title doesn't have the correct text.");
+    }
+
+    void verifySiteTitleLinkClick(WPFooter wpFooter) {
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
-        $(byXpath(footerSocialLinkedInXPath)).scrollTo().click();
+        wpFooter.siteTitleElement().scrollTo().click();
+        Assert.assertEquals(url(), menuHomeLink,"Site title link isn't correct.");
+    }
+
+    void verifyWPLink(WPFooter wpFooter) {
+        wpFooter.wordpressElement().should(exist).shouldBe(visible);
+        Assert.assertEquals(wpFooter.wordpressElementText(), footerWPLinkText,
+                "WordPress footer link doesn't have correct text.");
+    }
+
+    void verifyWPLinkClick(WPFooter wpFooter) {
+        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
+        wpFooter.wordpressElement().scrollTo().click();
+        Assert.assertEquals(url(), footerWPLink,"Clicking on Wordpress link doesn't go to expected URL.");
+    }
+
+    void verifySocialFacebookLink(WPFooter wpFooter) {
+        wpFooter.socialFacebookElement().should(exist).shouldBe(visible);
+        Assert.assertEquals(wpFooter.socialFacebookText(), footerSocialFacebookText,
+                "Facebook link doesn't have correct text.");
+    }
+
+    void verifySocialFacebookLinkClick(WPFooter wpFooter) {
+        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
+        wpFooter.socialFacebookElement().scrollTo().click();
+        Assert.assertEquals(url(), footerSocialFacebookLink,
+                "Clicking on Facebook link doesn't go to expected destination.");
+    }
+
+    void verifySocialTwitterLink(WPFooter wpFooter) {
+        wpFooter.socialTwitterElement().should(exist).shouldBe(visible);
+        Assert.assertEquals(wpFooter.socialTwitterText(), footerSocialTwitterText,
+                "Twitter link doesn't have correct text.");
+    }
+
+    void verifySocialTwitterLinkClick(WPFooter wpFooter) {
+        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
+        wpFooter.socialTwitterElement().scrollTo().click();
+        Assert.assertEquals(url(), footerSocialTwitterLink,
+                "Clicking on Twitter link doesn't go to correct destination.");
+    }
+
+    void verifySocialGithubLink(WPFooter wpFooter) {
+        wpFooter.socialGithubElement().should(exist).shouldBe(visible);
+        Assert.assertEquals(wpFooter.socialGithubText(), footerSocialGithubText,
+                "Github link doesn't have correct text.");
+    }
+
+    void verifySocialGithubLinkClick(WPFooter wpFooter) {
+        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
+        wpFooter.socialGithubElement().scrollTo().click();
+        Assert.assertEquals(url(), footerSocialGithubLink,
+                "Clicking on Github link doesn't go to expected destination.");
+    }
+
+    void verifySocialLinkedInLink(WPFooter wpFooter) {
+        wpFooter.socialLinkedInElement().should(exist).shouldBe(visible);
+        Assert.assertEquals(wpFooter.socialLinkedInText(), footerSocialLinkedInText,
+                "LinkedIn link doesn't have correct text.");
+    }
+
+    void verifySocialLinkedInLinkClick(WPFooter wpFooter) {
+        // Scrolling to the link is required, otherwise Selenide complains it's not clickable
+        wpFooter.socialLinkedInElement().scrollTo().click();
         // Can't doublecheck against my LinkedIn account because apparently Selenium triggers an auth wall?
         Assert.assertTrue(url().startsWith("https://www.linkedin.com"));
     }
