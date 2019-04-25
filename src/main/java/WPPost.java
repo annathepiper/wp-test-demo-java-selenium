f5b425dd085cfbc23dc96338b00cbc3bfb76aa1e
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-25 14:02:17 -0700

    Adding in WPMenu support
    Adding a WPMenu object as part of the overall structure for this class.

--- src/main/java/WPPost.java
+++ src/main/java/WPPost.java
@@ -1,10 +1,12 @@
 public class WPPost extends BaseTest {
+    WPMenu wpMenu;
     WPFooter wpFooter;
 
     /**
      * Constructor for the class.
      */
     WPPost() {
+        wpMenu = new WPMenu();
         wpFooter = new WPFooter();
     }
 }
