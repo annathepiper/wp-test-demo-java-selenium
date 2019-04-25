e6e918f5aef17933847052527ae78f4ca3c042be
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-25 14:45:19 -0700

    Removing this file
    Figured out a better way to do parent->child inheritance for the test cases so don't need this class anymore.

--- src/main/java/TestHomepageFooter.java
+++ src/main/java/TestHomepageFooter.java
@@ -11,8 +11,6 @@ import static com.codeborne.selenide.Selenide.open;
  * This test class extends the TestFooter class, and conducts footer tests against the homepage.
  */
 public class TestHomepageFooter extends TestFooter {
-    private WPFooter wpFooter;
-
     /**
      * Setup
      * This method opens up the homepage of the test site so we can do tests on it.
@@ -21,114 +19,6 @@ public class TestHomepageFooter extends TestFooter {
     public void methodSetup() {
         open(wpBaseUri);
         WPHomepage wpHomepage = new WPHomepage();
-        wpFooter = wpHomepage.wpFooter;
-    }
-
-    /**
-     * TestFooterSiteTitleLink
-     * Verify that the site title link is present, visible, and has the correct text.
-     */
-    @Test
-    public void TestFooterSiteTitleLink() {
-        verifySiteTitleLink(wpFooter);
-    }
-
-    /**
-     * TestFooterSiteTitleLinkClick
-     * Verify that if you click on the site title link, you land on the correct page (the homepage).
-     */
-    @Test
-    public void TestFooterSiteTitleLinkClick() {
-        verifySiteTitleLinkClick(wpFooter);
-    }
-
-    /**
-     * TestFooterWPLink
-     * Verify that the Wordpress link is present, visible, and has the correct text.
-     */
-    @Test
-    public void TestFooterWPLink() {
-        verifyWPLink(wpFooter);
-    }
-
-    /**
-     * TestFooterWPLinkClick
-     * Verify that if you click on the Wordpress link, you land on the correct page (wordpress.org).
-     */
-    @Test
-    public void TestFooterWPLinkClick() {
-        verifyWPLinkClick(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialFacebookLink
-     * Verify that the Facebook link is present, visible, and has the correct text.
-     */
-    @Test
-    public void TestFooterSocialFacebookLink() {
-        verifySocialFacebookLink(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialFacebookLinkClick
-     * Verify that if you click on the Facebook link, you land on the correct page (my Facebook account).
-     */
-    @Test
-    public void TestFooterSocialFacebookLinkClick() {
-        verifySocialFacebookLinkClick(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialTwitterLink
-     * Verify that the Twitter link is present, visible, and has the correct text.
-     */
-    @Test
-    public void TestFooterSocialTwitterLink() {
-        verifySocialTwitterLink(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialTwitterLinkClick
-     * Verify that if you click on the Twitter link, you land on the correct page (my Twitter account).
-     */
-    @Test
-    public void TestFooterSocialTwitterLinkClick() {
-        verifySocialTwitterLinkClick(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialGithubLink
-     * Verify that the Github link is present, visible, and has the correct text.
-     */
-    @Test
-    public void TestFooterSocialGithubLink() {
-        verifySocialGithubLink(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialGithubLinkClick
-     * Verify that if you click on the Github link, you land on the correct page (my Github account).
-     */
-    @Test
-    public void TestFooterSocialGithubLinkClick() {
-        verifySocialGithubLinkClick(wpFooter);
-    }
-
-    /**
-     * TestFooterSocialLinkedInLink
-     * Verify that the LinkedIn link is present, visible, and has the correct text.
-     */
-    @Test
-    public void TestFooterSocialLinkedInLink() {
-        verifySocialLinkedInLink(wpFooter);
-    }
-
-    /**
-     * TestSocialLinkedInLinkClick
-     * Verify that if you click on the LinkedIn link, you land on the correct page (my LinkedIn account).
-     */
-    @Test
-    public void TestSocialLinkedInLinkClick() {
-        verifySocialLinkedInLinkClick(wpFooter);
+        super.wpFooter = wpHomepage.wpFooter;
     }
 }
