73af7cf16540d87baa9c3a7ad27ec92b1da72de9
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-25 19:12:36 -0700

    Reorganizing test cases
    Condensing several test cases and organizing by specific areas of the footer being tested. This is to cut down on the overall number of test cases run in the parent class as well as its children.

--- src/main/java/TestFooter.java
+++ src/main/java/TestFooter.java
@@ -32,22 +32,15 @@ public class TestFooter extends BaseTest {
 
     /**
      * TestFooterSiteTitleLink
-     * Verify that the site title link is present, visible, and has the correct text.
+     * Verify that the site title link is present, visible, and has the correct text and destination.
      */
     @Test
     public void TestFooterSiteTitleLink() {
         wpLogger.info(String.format("Testing the footer site title link on: %s",targetUri));
+
         wpFooter.siteTitleElement().should(exist).shouldBe(visible);
         Assert.assertEquals(wpFooter.siteTitleText(), siteTitle,"Site title doesn't have the correct text.");
-    }
 
-    /**
-     * TestFooterSiteTitleLinkClick
-     * Verify that if you click on the site title link, you land on the correct page (the homepage).
-     */
-    @Test
-    public void TestFooterSiteTitleLinkClick() {
-        wpLogger.info(String.format("Testing clicking the footer site title link on: %s",targetUri));
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
         wpFooter.siteTitleElement().scrollTo().click();
         Assert.assertEquals(url(), menuHomeLink,"Site title link isn't correct.");
@@ -55,7 +48,7 @@ public class TestFooter extends BaseTest {
 
     /**
      * TestFooterWPLink
-     * Verify that the Wordpress link is present, visible, and has the correct text.
+     * Verify that the Wordpress link is present, visible, and has the correct text and destination.
      */
     @Test
     public void TestFooterWPLink() {
@@ -63,15 +56,7 @@ public class TestFooter extends BaseTest {
         wpFooter.wordpressElement().should(exist).shouldBe(visible);
         Assert.assertEquals(wpFooter.wordpressElementText(), footerWPLinkText,
                 "WordPress footer link doesn't have correct text.");
-    }
 
-    /**
-     * TestFooterWPLinkClick
-     * Verify that if you click on the Wordpress link, you land on the correct page (wordpress.org).
-     */
-    @Test
-    public void TestFooterWPLinkClick() {
-        wpLogger.info(String.format("Testing clicking the footer WordPress link on: %s",targetUri));
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
         wpFooter.wordpressElement().scrollTo().click();
         Assert.assertEquals(url(), footerWPLink,"Clicking on Wordpress link doesn't go to expected URL.");
@@ -87,15 +72,7 @@ public class TestFooter extends BaseTest {
         wpFooter.socialFacebookElement().should(exist).shouldBe(visible);
         Assert.assertEquals(wpFooter.socialFacebookText(), footerSocialFacebookText,
                 "Facebook link doesn't have correct text.");
-    }
 
-    /**
-     * TestFooterSocialFacebookLinkClick
-     * Verify that if you click on the Facebook link, you land on the correct page (my Facebook account).
-     */
-    @Test
-    public void TestFooterSocialFacebookLinkClick() {
-        wpLogger.info(String.format("Testing clicking the footer Facebook link on: %s",targetUri));
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
         wpFooter.socialFacebookElement().scrollTo().click();
         Assert.assertEquals(url(), footerSocialFacebookLink,
@@ -112,15 +89,7 @@ public class TestFooter extends BaseTest {
         wpFooter.socialTwitterElement().should(exist).shouldBe(visible);
         Assert.assertEquals(wpFooter.socialTwitterText(), footerSocialTwitterText,
                 "Twitter link doesn't have correct text.");
-    }
 
-    /**
-     * TestFooterSocialTwitterLinkClick
-     * Verify that if you click on the Twitter link, you land on the correct page (my Twitter account).
-     */
-    @Test
-    public void TestFooterSocialTwitterLinkClick() {
-        wpLogger.info(String.format("Testing clicking the footer Twitter link on: %s",targetUri));
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
         wpFooter.socialTwitterElement().scrollTo().click();
         Assert.assertEquals(url(), footerSocialTwitterLink,
@@ -137,15 +106,7 @@ public class TestFooter extends BaseTest {
         wpFooter.socialGithubElement().should(exist).shouldBe(visible);
         Assert.assertEquals(wpFooter.socialGithubText(), footerSocialGithubText,
                 "Github link doesn't have correct text.");
-    }
 
-    /**
-     * TestFooterSocialGithubLinkClick
-     * Verify that if you click on the Github link, you land on the correct page (my Github account).
-     */
-    @Test
-    public void TestFooterSocialGithubLinkClick() {
-        wpLogger.info(String.format("Testing clicking the footer Github link on: %s",targetUri));
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
         wpFooter.socialGithubElement().scrollTo().click();
         Assert.assertEquals(url(), footerSocialGithubLink,
@@ -162,15 +123,7 @@ public class TestFooter extends BaseTest {
         wpFooter.socialLinkedInElement().should(exist).shouldBe(visible);
         Assert.assertEquals(wpFooter.socialLinkedInText(), footerSocialLinkedInText,
                 "LinkedIn link doesn't have correct text.");
-    }
 
-    /**
-     * TestSocialLinkedInLinkClick
-     * Verify that if you click on the LinkedIn link, you land on the correct page (my LinkedIn account).
-     */
-    @Test
-    public void TestSocialLinkedInLinkClick() {
-        wpLogger.info(String.format("Testing clicking the footer LinkedIn link on: %s",targetUri));
         // Scrolling to the link is required, otherwise Selenide complains it's not clickable
         wpFooter.socialLinkedInElement().scrollTo().click();
         // Can't doublecheck against my LinkedIn account because apparently Selenium triggers an auth wall?
