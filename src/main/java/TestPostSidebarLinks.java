9ef3497e6d09b98781ad639dc8cd31357ce73fd3
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-26 14:54:06 -0700

    Updating to reflect refactor of TestSidebarLinks class
    Did appropriate tweaks to this class to make it a proper child of TestSidebarLinks, as per work done in other test classes.

--- src/main/java/TestPostSidebarLinks.java
+++ src/main/java/TestPostSidebarLinks.java
@@ -1,4 +1,4 @@
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.open;
  * TestPostSidebarLinks
  * @author Angela Korra'ti
  *
- * Last updated 4/25/2019
+ * Last updated 4/26/2019
  * This class conducts tests against the sidebar links on a post of the test WordPress site. Inherits its tests from the
  * TestSidebarLinks class.
  */
@@ -16,8 +16,11 @@ public class TestPostSidebarLinks extends TestSidebarLinks {
      * Setup
      * This method opens up a post of the test site so we can do tests on it.
      */
-    @BeforeClass
-    public void classSetup() {
+    @BeforeMethod
+    public void methodSetup() {
         open(wpPostUri);
+        super.targetUri = wpPostUri;
+        WPPost wpPost = new WPPost();
+        super.wpSidebar = wpPost.wpSidebar;
     }
 }
