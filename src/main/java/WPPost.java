0b2228018996ad12b8f2d32ea0bfe0e5fcbfd6be
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-26 14:57:45 -0700

    Adding WPSidebar object to class structure, other changes
    Adding a sidebar object as a part of this class structure, and also the class comment block I was missing. Put in a few initial elements useful in the sidebar links tests for checking the title of a post.

--- src/main/java/WPPost.java
+++ src/main/java/WPPost.java
@@ -1,5 +1,18 @@
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * WPPost
+ * @author Angela Korra'ti
+ *
+ * Last updated 4/26/2019
+ * This is the WPPost helper class that defines the structure of a post object.
+ */
 public class WPPost extends BaseTest {
     WPMenu wpMenu;
+    WPSidebar wpSidebar;
     WPFooter wpFooter;
 
     /**
@@ -7,6 +20,23 @@ public class WPPost extends BaseTest {
      */
     WPPost() {
         wpMenu = new WPMenu();
+        wpSidebar = new WPSidebar();
         wpFooter = new WPFooter();
+    }
+
+    /**
+     * postTitleElement
+     * @return Selenide element representing the title of the post
+     */
+    SelenideElement postTitleElement() {
+        return $(byClassName(entryTitleClass));
+    }
+
+    /**
+     * postTitleText
+     * @return String containing the title of the post
+     */
+    String postTitleText() {
+        return postTitleElement().text();
     }
 }
