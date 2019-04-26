81b6c140f060d9a0298a6197e14ad4296261126e
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-26 14:53:51 -0700

    Updating to reflect refactor of TestSidebar class
    Did appropriate tweaks to this class to make it a proper child of TestSidebar, as per work done in other test classes.

--- src/main/java/TestPostSidebar.java
+++ src/main/java/TestPostSidebar.java
@@ -1,12 +1,11 @@
-import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.BeforeMethod;
 import static com.codeborne.selenide.Selenide.open;
 
 /**
  * TestPostSidebar
  * @author Angela Korra'ti
  *
- * Last updated 4/25/2019
+ * Last updated 4/26/2019
  * This class conducts tests against the sidebar on a post of the test WordPress site. Inherits its tests from the
  * TestSidebar class.
  */
@@ -16,8 +15,11 @@ public class TestPostSidebar extends TestSidebar {
      * Setup
      * This method opens up a post of the test site so we can do tests on it.
      */
-    @BeforeClass
-    public void classSetup() {
+    @BeforeMethod
+    public void methodSetup() {
         open(wpPostUri);
+        super.targetUri = wpPostUri;
+        WPPost wpPost = new WPPost();
+        super.wpSidebar = wpPost.wpSidebar;
     }
 }
