fe4ec2710f426d5e7253d3aaaffc4afd83027893
Angela Korra'ti <annathepiper@fastmail.com>
2019-05-09 14:19:32 -0700

    Additional class elements needed
    Adding in the additional elements for the base post tests.

--- src/main/java/WPPost.java
+++ src/main/java/WPPost.java
@@ -1,13 +1,14 @@
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.$;
 
 /**
  * WPPost
  * @author Angela Korra'ti
  *
- * Last updated 4/26/2019
+ * Last updated 5/9/2019
  * This is the WPPost helper class that defines the structure of a post object.
  */
 public class WPPost extends BaseTest {
@@ -22,6 +23,62 @@ public class WPPost extends BaseTest {
         wpMenu = new WPMenu();
         wpSidebar = new WPSidebar();
         wpFooter = new WPFooter();
+    }
+
+    /**
+     * siteTitleElement
+     * @return Selenide element that refers to the site title
+     */
+    SelenideElement siteTitleElement() {
+        return $(byClassName(siteTitleClass));
+    }
+
+    /**
+     * siteTitle
+     * @return String containing the text of the site title
+     */
+    String siteTitle() {
+        return siteTitleElement().text();
+    }
+
+    /**
+     * siteDescriptionElement
+     * @return Selenide element that refers to the site title
+     */
+    SelenideElement siteDescriptionElement() {
+        return $(byClassName(siteDescriptionClass));
+    }
+
+    /**
+     * siteDescription
+     * @return String containing the text of the site description
+     */
+    String siteDescription() {
+        return siteDescriptionElement().text();
+    }
+
+    /**
+     * contentAreaElement
+     * @return Selenide element that refers to the content area
+     */
+    SelenideElement contentAreaElement() {
+        return $(byId(contentId));
+    }
+
+    /**
+     * primaryContentAreaElement
+     * @return Selenide element that refers to the primary content area
+     */
+    SelenideElement primaryContentAreaElement() {
+        return $(byId(primaryContentId));
+    }
+
+    /**
+     * secondaryContentAreaElement
+     * @return Selenide element that refers to the secondary content area
+     */
+    SelenideElement secondaryContentAreaElement() {
+        return $(byId(secondaryContentId));
     }
 
     /**
